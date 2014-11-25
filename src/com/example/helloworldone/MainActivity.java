1138a2c06c82af5e089b71a1a85d30610a3d66b3
PC-Lab <PC-Lab@uni-klu>
2014-11-25 16:55:39 +0100

    Totally wrong version

--- src/com/example/helloworldone/MainActivity.java
+++ src/com/example/helloworldone/MainActivity.java
@@ -20,6 +20,20 @@ public class MainActivity extends Activity {
     
     
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
